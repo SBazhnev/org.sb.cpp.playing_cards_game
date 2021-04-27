3494de2d6cd2ebe70b75e81195f8d418ce82ee2d
Sergey Bazhnev <SBazhnev@gmail.com>
2021-04-27 20:02:33 +0700

    Add Game class
    Only definition.

---
+++ src/game.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 Sergey Bazhnev <SBazhnev@gmail.com>
+// 
+//
+
+#include "game.h"
+
+namespace playingcards {
+
+void Game::Run()
+{
+}
+
+void Game::ModeSimpleRun()
+{
+}
+
+void Game::ModeTwoPlayersRun()
+{
+}
+
+void Game::Exit()
+{
+}
+
+} // namespace playingcards
---
+++ src/game.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2021 Sergey Bazhnev <SBazhnev@gmail.com>
+// 
+//
+
+#ifndef GAME_H_
+#define GAME_H_
+
+namespace playingcards {
+
+class Game {
+public:
+  Game() = default;
+
+  ~Game() = default;
+
+  Game(const Game&) = default;
+  Game& operator=(const Game&) = default;
+
+  Game(Game&&) = default;
+  Game& operator=(Game&&) = default;
+
+  void Run();
+
+private:
+  void ModeSimpleRun();
+  void ModeTwoPlayersRun();
+  void Exit();
+
+};
+
+} // namespace playingcards
+
+#endif // GAME_H_
